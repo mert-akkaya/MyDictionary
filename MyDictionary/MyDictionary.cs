25ac5911d6bc1e45309d20ced45c603c778e3a4c
Mert AKKAYA <mertakkaya522@gmail.com>
2021-01-28 18:57:17 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Key,Value>
+    {
+        Key[] _key;
+        Value[] _value;
+
+        public MyDictionary()
+        {
+            _key = new Key[0];
+            _value = new Value[0];
+        }
+
+        public void Add(Key key,Value value)
+        {
+            Key[] tempKey = _key;
+            _key = new Key[_key.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                _key[i] = tempKey[i];
+            }
+            _key[_key.Length - 1] = key;
+            Value[] tempValue = _value;
+            _value = new Value[_value.Length + 1];
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                _value[i] = tempValue[i];
+            }
+            _value[_value.Length - 1] = value;
+            Console.WriteLine(" Plaka : " + key + " , "+ " Şehir : " +value);
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+            sehirler.Add(06, "Ankara");
+            sehirler.Add(34, "İstanbul");
+            sehirler.Add(35, "İzmir");        
+        }
+    }
+}
